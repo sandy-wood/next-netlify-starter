9670d782d9f0411bd124ce4ff0f2be5d1d8f5f65
Sandy Wood <sandywood101010@gmail.com>
2021-03-04 16:47:13 +0000

    Contentful test

---
+++ pages/test.js
@@ -0,0 +1,27 @@
+// at the top of your component file
+
+import { fetchEntries } from '@utils/contentfulPosts'
+import Post from '@components/Post'
+
+// inside your component markup, pull `posts` from props
+
+<div className="posts">
+  {posts.map((p) => {
+    return <Post key={p.date} date={p.date} image={p.image.fields} title={p.title} />
+  })}
+</div>
+
+// at the bottom of your component file
+
+export async function getStaticProps() {
+  const res = await fetchEntries()
+  const posts = await res.map((p) => {
+    return p.fields
+  })
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
